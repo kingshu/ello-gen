d6446418064a1a5d0ed443087292e26a6501fe37
Nelson Gomez <nsgomez@users.noreply.github.com>
2014-09-28 16:01:59 -0400

    Detect API throttling (Enhance Your Calm) to retry

--- ello-gen.sh
+++ ello-gen.sh
@@ -5,8 +5,17 @@ for i in $(cat words.txt); do
         for k in $(cat words.txt); do
             content="$(curl -s "$url/$i-$j-$k)")"
             torf="${content:13:1}"
+
+            while [ "$content" == "Enhance your calm" ]; do
+                echo "    > Waiting 10s and trying again..."
+                sleep 10
+
+                content="$(curl -s "$url/$i-$j-$k)")"
+                torf="${content:13:1}"
+            done
+
             if [ $torf == "t" ]; then
-                echo $i-$j-$k
+                echo "$i-$j-$k"
             fi
         done
     done
